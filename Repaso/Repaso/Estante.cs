a2b72ef5d09f0c4bf218c0b7378a70151f1462e9
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-09-04 22:13:38 -0300

    Ejercicio clase 5
    Completo, pero no funciona el test

---
+++ Repaso/Repaso/Estante.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repaso
+{
+    class Estante
+    {
+        private int ubicacionEstante;
+        private Producto[] productos;
+
+        private Estante(int capacidad)
+        {
+            productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion):this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder informacion = new StringBuilder();
+
+            foreach (Producto producto in e.productos)
+            {
+                informacion.AppendFormat("Codigo: {0}\nMarca: {1}\nPrecio: {2}", (String) producto, producto.GetMarca(), producto.GetPrecio());                 
+            }
+            return informacion.ToString();           
+        }
+
+        public static bool operator == (Estante estante, Producto producto)
+        {
+            bool seEncuentra = false; 
+
+            foreach(Producto prodEst in estante.productos)
+            {
+                if (prodEst == producto)
+                {
+                    seEncuentra = true;
+                    break;
+                }
+            }
+            return seEncuentra;
+        }
+
+        public static bool operator != (Estante estante, Producto producto)
+        {
+            return !(estante == producto);
+        }
+
+        public static bool operator + (Estante estante, Producto producto)
+        {
+            bool tieneCapacidad = false; 
+
+            if(estante != producto)
+            {
+                for(int i = 0; i < estante.productos.Length; i++)
+                {
+                    if(estante.productos[i] is null)
+                    {
+                        estante.productos[i] = producto;
+                        tieneCapacidad = true;
+                        break;
+                    }
+                }
+            }
+            return tieneCapacidad;
+        }
+
+        public static Estante operator - (Estante estante, Producto producto)
+        {
+            if(estante == producto)
+            {
+                for(int i = 0; i < estante.productos.Length; i++)
+                {
+                    if(estante.productos[i] == producto)
+                    {
+                        estante.productos[i] = null;
+                        break; 
+                    }
+                }
+            }
+            return estante;
+        }
+    }
+}
---
+++ Repaso/Repaso/Producto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repaso
+{
+    class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio; 
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.codigoDeBarra = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            string producto;
+            producto = String.Format("Codigo: {0} \nMarca: {1}\nPrecio: {2}", p.codigoDeBarra, p.marca, p.precio);
+            return producto;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            string codigo = p.codigoDeBarra;
+            return codigo;
+        }
+        
+        public static bool operator == (Producto p1, Producto p2)
+        {
+            return p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra;
+        }
+
+        public static bool operator != (Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+
+    }
+}
