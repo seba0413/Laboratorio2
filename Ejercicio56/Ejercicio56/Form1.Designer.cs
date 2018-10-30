6679f3a19127559cfed13d4485977161497f54e4
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-10-29 22:15:39 -0300

    Ejercicio 56

---
+++ Ejercicio56/Ejercicio56/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Ejercicio56
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuArchivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAbrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuGuardar = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuGuardarComo = new System.Windows.Forms.ToolStripMenuItem();
+            this.stsCaracteres = new System.Windows.Forms.StatusStrip();
+            this.rtbTexto = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuArchivo});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(466, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuArchivo
+            // 
+            this.mnuArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAbrir,
+            this.mnuGuardar,
+            this.mnuGuardarComo});
+            this.mnuArchivo.Name = "mnuArchivo";
+            this.mnuArchivo.Size = new System.Drawing.Size(60, 20);
+            this.mnuArchivo.Text = "Archivo";
+            this.mnuArchivo.Click += new System.EventHandler(this.archivoToolStripMenuItem_Click);
+            // 
+            // mnuAbrir
+            // 
+            this.mnuAbrir.Name = "mnuAbrir";
+            this.mnuAbrir.Size = new System.Drawing.Size(180, 22);
+            this.mnuAbrir.Text = "Abrir";
+            this.mnuAbrir.Click += new System.EventHandler(this.mnuAbrir_Click);
+            // 
+            // mnuGuardar
+            // 
+            this.mnuGuardar.Name = "mnuGuardar";
+            this.mnuGuardar.Size = new System.Drawing.Size(180, 22);
+            this.mnuGuardar.Text = "Guardar";
+            this.mnuGuardar.Click += new System.EventHandler(this.mnuGuardar_Click);
+            // 
+            // mnuGuardarComo
+            // 
+            this.mnuGuardarComo.Name = "mnuGuardarComo";
+            this.mnuGuardarComo.Size = new System.Drawing.Size(180, 22);
+            this.mnuGuardarComo.Text = "Guardar como...";
+            this.mnuGuardarComo.Click += new System.EventHandler(this.mnuGuardarComo_Click);
+            // 
+            // stsCaracteres
+            // 
+            this.stsCaracteres.Location = new System.Drawing.Point(0, 214);
+            this.stsCaracteres.Name = "stsCaracteres";
+            this.stsCaracteres.Size = new System.Drawing.Size(466, 22);
+            this.stsCaracteres.TabIndex = 1;
+            this.stsCaracteres.Text = "statusStrip1";
+            // 
+            // rtbTexto
+            // 
+            this.rtbTexto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbTexto.Location = new System.Drawing.Point(0, 24);
+            this.rtbTexto.Name = "rtbTexto";
+            this.rtbTexto.Size = new System.Drawing.Size(466, 190);
+            this.rtbTexto.TabIndex = 2;
+            this.rtbTexto.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 236);
+            this.Controls.Add(this.rtbTexto);
+            this.Controls.Add(this.stsCaracteres);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuArchivo;
+        private System.Windows.Forms.ToolStripMenuItem mnuAbrir;
+        private System.Windows.Forms.ToolStripMenuItem mnuGuardar;
+        private System.Windows.Forms.ToolStripMenuItem mnuGuardarComo;
+        private System.Windows.Forms.StatusStrip stsCaracteres;
+        private System.Windows.Forms.RichTextBox rtbTexto;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Ejercicio56/Ejercicio56/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ejercicio56
+{
+    public partial class Form1 : Form
+    {
+        string archivo;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.mnuGuardar.Enabled = false;
+        }
+
+        private void archivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mnuAbrir_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Archivo de texto |*txt|Archivo DAT|*.dat;*.bin";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {                
+                archivo = openFileDialog1.FileName;
+                this.mnuGuardar.Enabled = true;
+                StreamReader sr = new StreamReader(archivo);
+                rtbTexto.Text = sr.ReadToEnd();
+                sr.Close();
+            }            
+        }
+
+        private void mnuGuardar_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(archivo);
+            sw.Write(rtbTexto.Text);
+            sw.Close();
+        }
+
+        private void mnuGuardarComo_Click(object sender, EventArgs e)
+        {
+            string archivo;
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                archivo = saveFileDialog1.FileName;
+                StreamWriter sw = new StreamWriter(archivo);
+                sw.Write(rtbTexto.Text);
+                sw.Close();
+            }
+        }
+    }
+}
