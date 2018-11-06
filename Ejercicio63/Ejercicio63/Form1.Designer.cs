f726dd68df9958d8267973b05f668358092496e4
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-11-06 20:11:14 -0300

    Ejercicio 63

---
+++ Ejercicio63/Ejercicio63/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Ejercicio63
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(13, 13);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(35, 13);
+            this.lblHora.TabIndex = 0;
+            this.lblHora.Text = "label1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(275, 232);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lblHora);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Ejercicio63/Ejercicio63/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Ejercicio63
+{
+    public partial class Form1 : Form
+    {
+        Thread t1;
+   
+        public Form1()
+        {
+            InitializeComponent();
+            t1 = new Thread(AsignarHora);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           t1.Start();           
+        }
+
+        private void AsignarHora()
+        {
+
+            while (true)
+            {
+                DateTime dt = new DateTime();
+                dt = DateTime.Now;
+                string fechaActual = dt.ToString("dd/MM/yyyy hh:mm:ss");
+                if (this.lblHora.InvokeRequired)
+                {
+                    this.lblHora.BeginInvoke((MethodInvoker)delegate()
+                    {
+                        this.lblHora.Text = fechaActual;
+                    }
+                    );
+                }
+                else
+                {
+                    this.lblHora.Text = fechaActual;
+                }
+
+                Thread.Sleep(1000);
+            }
+
+                   
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
