52e4e7d52df527ae643b48006ac6d69e491abd06
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-11-06 21:25:25 -0300

    Ejercicio 64

---
+++ Ejercicio64/Ejercicio64/Caja.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ejercicio64
+{
+    public class Caja
+    {
+        private List<String> filaClientes;
+
+        public Caja()
+        {
+            filaClientes = new List<string>();
+        }
+
+        public List<String> FilaClientes
+        {
+            get
+            {
+                return this.filaClientes;
+            }
+        }
+
+        public void AtenderClientes()
+        {
+            foreach(string cliente in filaClientes)
+            {
+                Console.WriteLine(String.Format("{0}, Cliente {1}\n", Thread.CurrentThread.Name, cliente));
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ Ejercicio64/Ejercicio64/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Ejercicio64
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Caja caja1 = new Caja();
+            Caja caja2 = new Caja();
+            Negocio negocio = new Negocio(caja1, caja2);
+
+            negocio.Clientes.Add("cliente1");
+            negocio.Clientes.Add("cliente2");
+            negocio.Clientes.Add("cliente3");
+            negocio.Clientes.Add("cliente4");
+            negocio.Clientes.Add("cliente5");
+            negocio.Clientes.Add("cliente6");
+            negocio.Clientes.Add("cliente7");
+            negocio.Clientes.Add("cliente8");
+
+            Thread t1 = new Thread(caja1.AtenderClientes);
+            Thread t2 = new Thread(caja2.AtenderClientes);
+            Thread t3 = new Thread(negocio.AsignarCaja);
+
+            t3.Start();
+            t3.Join();
+            t1.Name = "Caja 1";
+            t1.Start();
+            t2.Name = "Caja 2";
+            t2.Start();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
